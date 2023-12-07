1861900b3c120eccb0bab8f9726846d630965a88
ferchoeth <ferchoeth@gmail.com>
2023-12-07 17:57:45 -0600

    v1

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions = {
+  // pages: {
+  //   signIn: "/login",
+  // },
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'https://www.googleapis.com/auth/gmail.readonly openid email profile ',
+        },
+      },
+    })
+  ],
+  callbacks: {
+    jwt: async ({ token, account }) => {
+      if (account?.access_token) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    }
+  }
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ app/api/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import { getUserSession } from '@/lib/session'
+
+export async function GET(req) {
+  const user = await getUserSession()
+  if(!user) {
+    const loginUrl = new URL('/api/auth/signin', req.url);
+    return NextResponse.redirect(loginUrl);
+  } else {
+    const userId = user.email;
+    const data = await fetch(`https://gmail.googleapis.com/gmail/v1/users/${userId}/messages?maxResults=3`, {
+      headers: {
+        'Authorization': `Bearer ${user.access_token}`
+      }
+    })
+      .then(res => res.json())
+    return NextResponse.json({ data });
+  }
+}
