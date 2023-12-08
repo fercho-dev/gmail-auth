e0432e58437b8f6cd7f73cf198b05e4fa0a83b6e
ferchoeth <ferchoeth@gmail.com>
2023-12-07 18:54:30 -0600

    v1.1

--- app/api/route.js
+++ app/api/route.js
@@ -4,8 +4,10 @@ import { getUserSession } from '@/lib/session'
 export async function GET(req) {
   const user = await getUserSession()
   if(!user) {
-    const loginUrl = new URL('/api/auth/signin', req.url);
-    return NextResponse.redirect(loginUrl);
+    // const loginUrl = new URL('/api/auth/signin', req.url);
+    // return NextResponse.redirect(loginUrl);
+    // return error
+    return NextResponse.error(new Error('Not authorized'), { status: 401 });
   } else {
     const userId = user.email;
     const data = await fetch(`https://gmail.googleapis.com/gmail/v1/users/${userId}/messages?maxResults=3`, {
