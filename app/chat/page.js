a86396d172f1c0d59eaa8a08c0e9df4a209817f1
ferchoeth <ferchoeth@gmail.com>
2024-01-09 21:15:20 -0600

    fix expired token bug

--- app/chat/page.js
+++ app/chat/page.js
@@ -1,10 +1,39 @@
 "use client";
 
-import React from "react";
 import { Conversation } from "../components/Conversation";
 import { Header } from "../components/Header";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
+import { signOut } from 'next-auth/react';
 
 function Chat() {
+  const router = useRouter();
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const response = await fetch("/api/user");
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const userData = await response.json();
+        if(!userData.name) {
+          router.push('/');
+        }
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (userData.expires_at <= currentTime) {
+          await signOut({ redirect: false });
+          router.push('/');
+        }
+      } catch (error) {
+        console.error('Error fetching user:', error);
+        router.push('/');
+      }
+    }
+
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Header />
